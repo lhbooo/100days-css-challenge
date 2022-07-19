d33e918a0ec858fb1c3d72a291c12ba5c1471a8c
haibolian <51857421+haibolian@users.noreply.github.com>
2022-07-19 07:04:31 +0000

    Deploying to gh-pages from @ haibolian/100days-css-challenge@59167b7d7d5ebd7c0ec9bb97b47b7b97aa8f0759 🚀

---
+++ assets/index.773d262a.js
@@ -0,0 +1,1 @@
+import{a as s,j as a}from"./index.9b3a10e9.js";const e=()=>s("div",{className:"frame day3",children:[s("div",{className:"wapper",children:[a("div",{className:"sky"}),a("div",{className:"sun"}),a("div",{className:"hill-left"}),a("div",{className:"hill-right"}),a("div",{className:"hill-shadow"}),a("div",{className:"ground"})]}),a("div",{className:"mask"})]});export{e as default};
