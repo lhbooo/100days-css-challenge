3fc52efcfac32968137b49396a7972b4a1b04e79
lianhaibo <957377783@qq.com>
2022-08-15 21:15:16 +0800

    chore: 项目响应式布局

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const Day7 = React.lazy(() => import('./challenge/day7'));
 const Day8 = React.lazy(() => import('./challenge/day8'));
 const Day9 = React.lazy(() => import('./challenge/day9'));
 const Day10 = React.lazy(() => import('./challenge/day10'));
+const Day11 = React.lazy(() => import('./challenge/day11'));
 
 function App() {
   return (
@@ -19,18 +20,21 @@ function App() {
         <h1>100 days css challenge</h1>
       </header>
       <main className='challenge'>
-        <Day1></Day1>
-        <Day2></Day2>
-        <Day3></Day3>
-        <Day4></Day4>
-        <Day5></Day5>
-        <Day6></Day6>
-        <Day7></Day7>
-        <Day8></Day8>
-        <Day9></Day9>
-        <Day10></Day10>
+        <div className='challenge-items'>
+          <Day1></Day1>
+          <Day2></Day2>
+          <Day3></Day3>
+          <Day4></Day4>
+          <Day5></Day5>
+          <Day6></Day6>
+          <Day7></Day7>
+          <Day8></Day8>
+          <Day9></Day9>
+          <Day10></Day10>
+          <Day11></Day11>
+        </div>
       </main>
-      {/* <iframe height={'500px'} width={'500px'} src='https://cdpn.io/roydigerhund/fullembedgrid/MKxgWz?animations=run&type=embed'/> */}
+      {/* <iframe height={'500px'} width={'500px'} src='https://cdpn.io/roydigerhund/fullembedgrid/GZpzEE?animations=run&type=embed'/> */}
     </div>
   )
 }
