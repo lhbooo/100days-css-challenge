a7776ce4fcb1cf11a7fce90056260681dc06be78
lianhaibo <lianhaibo@tele-hot.com>
2024-05-17 14:45:36 +0800

    fanish day14

--- src/challenge/day14/index.jsx
+++ src/challenge/day14/index.jsx
@@ -1,16 +1,51 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './index.scss';
 
 const Day14 = () => {
+  const numberRef = useRef(null)
   const [count, setCount] = useState(0)
+  const [preCount, setPreCount] = useState(0)
+
+  const decreaseCount = () => {
+    const dom = numberRef.current
+    dom.innerHTML = ''
+    const fadeOut = document.createElement('span')
+    fadeOut.className = 'fade-out';
+    fadeOut.innerText = count;
+
+    const fadeIn = document.createElement('span')
+    fadeIn.className = 'fade-in';
+    fadeIn.innerText = count - 1;
+    setCount(count - 1)
+    dom.append(fadeOut);
+    dom.append(fadeIn);
+  }
+
+  const increaseCount = () => {
+    const dom = numberRef.current
+    dom.innerHTML = ''
+    const fadeOut = document.createElement('span')
+    fadeOut.className = 'fade-out';
+    fadeOut.innerText = count;
+
+    const fadeIn = document.createElement('span')
+    fadeIn.className = 'fade-in';
+    fadeIn.innerText = count + 1;
+    setCount(count + 1)
+    dom.append(fadeOut);
+    dom.append(fadeIn);
+  }
+
   return (
     <div className="frame day14">
       <div className='count-button'>
-        <div className='count-button__decrease' onClick={() => setCount(count - 1)}>
+        <div className='count-button__decrease' onClick={decreaseCount}>
           <div className='line'></div>
         </div>
-        <div className='count-button__number'>{ count }</div>
-        <div className='count-button__increase' onClick={() => setCount(count + 1)}>
+        <div className='count-button__number' ref={numberRef}>
+
+        </div>
+        <div className='count-button__increase' onClick={increaseCount}>
           <div className='plus'>
             <div className='plus-h'></div>
             <div className='plus-v'></div>
